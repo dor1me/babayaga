a081ff238c47eadf9482577ae0de69d38ac2ece8
Fedor Tarasov <u9611@yandex.ru>
2023-07-22 01:19:04 +0300

    kolobok movement

--- scripts/small_kolobok.gd
+++ scripts/small_kolobok.gd
@@ -7,16 +7,15 @@ const JUMP_VELOCITY = -400.0
 var speed = G.small_kolobok_speed
 var attack = G.small_kolobok_attack
 var hp = G.small_kolobok_hp
-var player_posx = G.player_posx
-@onready var kolobok_posx = $Node2D
+@onready var player = $"../player"
 @onready var body = $CollisionShape2D
 
 
 
 
 func _physics_process(delta):
-	if position.x < player_posx:
-		position.x += SPEED / 10 
-	elif position.x > player_posx:
-		position.x -= SPEED / 10 
+	if position.x < player.position.x:
+		position.x += SPEED / 100 
+	elif position.x > player.position.x:
+		position.x -= SPEED / 100 
 
