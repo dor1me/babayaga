136dcfe03493ca4648ce3d2062eefc69e7d950e2
Fedor Tarasov <u9611@yandex.ru>
2023-07-22 21:22:20 +0300

    font size in dialog

--- levels/dialogs/hotline_dialog.gd
+++ levels/dialogs/hotline_dialog.gd
@@ -4,21 +4,20 @@ extends Node2D
 
 func create_label(str):
 	var label = Label.new()
-	
-	
-	label.position.x = 10
-	label.text = str
-	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART;
-	label.size.y = label.get_line_height()*label.get_line_count()
-	label.size.x = chat.size.x - 20
-	
+	label.add_theme_font_size_override("font_size", 32)
 	chat.add_child(label)
+	label.position.x = 10
+	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART;
+	label.text = str
+	
+	label.size.x = chat.size.x - 20
+	label.size.y = label.get_line_height()*label.get_line_count()
 	
 	# move up
 	for child in chat.get_children(false):
 		if child == label:
 			continue
-		child.position.y -= label.size.y + 20;
+		child.position.y -= label.size.y + label.get_line_height();
 
 	label.position.y = chat.size.y - label.size.y
 	
