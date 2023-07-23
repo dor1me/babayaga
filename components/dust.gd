0a4fd2cc7d692e4cfaa97fe82899efa1d2123f02
Fedor Tarasov <u9611@yandex.ru>
2023-07-23 20:59:06 +0300

    added dust

--- components/dust.gd
+++ components/dust.gd
@@ -4,16 +4,17 @@ class_name Dust
 var opacity = 0.7
 var radius = 2
 var velocity = Vector2(-0.4,0)
+var color = Color(0.5, 0.5, 0.5)
 func _draw():
 	var center = Vector2(0, 0)
 	
-	var color = Color(0.5, 0.5, 0.5, opacity)
+	
 
-	draw_circle( center, radius, color )
+	draw_circle( center, radius, Color(color, opacity) )
 
 func _process(delta):
-	opacity -= 0.2*delta
-	radius += 5*delta
+	opacity -= 0.1*delta
+	radius += 10*delta
 	queue_redraw()
 	velocity.y += 0.05*delta
 	velocity.x += 0.075*delta
--- levels/underlevel.gd
+++ levels/underlevel.gd
@@ -5,8 +5,13 @@ var dust_class = preload("res://components/dust.tscn")
 func _on_button_pressed():
 	for i in 50:
 		var dust_node = dust_class.instantiate()
-		var offset = Vector2(randi()%20-10,randi()%20-10)
-		dust_node.velocity = offset*0.05
+		
+		dust_node.velocity = Vector2(randi()%10 * 0.1,0.2)
+		dust_node.radius = 15
+		dust_node.opacity = 0.3
+		var color_offset = (randf()-0.5)*0.2
+		dust_node.color = Color(0.5+color_offset, 0.5+color_offset, 0.5+color_offset)
+		var offset = Vector2(randi()%40-20,randi()%40-20)
 		dust_node.position = Vector2(600,100)+offset
 		get_tree().get_root().add_child(dust_node)
 	
