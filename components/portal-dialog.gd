e4b3a3d3b48abecdac831fe98fbd63078f6d129f
Fedor Tarasov <u9611@yandex.ru>
2023-07-23 10:28:27 +0300

    fixed gialog-portal

--- components/portal-dialog.gd
+++ components/portal-dialog.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export_enum("kolobok","vodyanoy","irka") var dialog: String
 
-func _on_body_exited(body):
+
+func _on_body_entered(body):
 	if body.name == "player":
 		G.goto_dialog(dialog)
