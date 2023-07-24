d387a0d1351c15b541ca892a6ea681369835747d
chartychi <chartychi@gmail.com>
2023-07-24 07:30:22 +0300

    kolobok level

---
+++ scripts/kolobok_level.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var count_enemies = 5
+@onready var kol1 = $small_kolobor
+@onready var kol2 = $small_kolobor2
+@onready var kol3 = $small_kolobor3
+@onready var kol4 = $small_kolobor4
+@onready var kol5 = $small_kolobor5
+func _ready():
+	pass 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
